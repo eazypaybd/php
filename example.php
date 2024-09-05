d3b93c109ab3ed08fa1a62dd960d2f43814cc775
eazypaybd <eazypaybd.com@gmail.com>
2024-09-05 12:01:29 -0700

    Update example.php

--- example.php
+++ example.php
@@ -1,2 +1,18 @@
 <?php
 require_once('mobilerecharge.php');
+$apikey = "your api key";
+$secretkey = "your secret key";
+$mobilerecharge = mobilerecharge($apikey, $secretkey);
+
+// balance chack start
+echo $Recharge->chack($params);
+// balance chack end
+
+// recharge requst start
+$params = array();
+$params['mobile'] = "01711111111";
+$params['amount'] = "20";
+$params['oparetor'] = "gp";
+$params['type'] = "prepaid";
+echo $Recharge->recharge($params);
+// recharge requst end
